c5d7e6665d16d5c872f87c28f243515718781966
captainche1 <807100784v@gmail.com>
2019-08-11 21:07:47 +0800

    first commit

---
+++ src/main/java/com/che/comminuty/ComminutyApplication.java
@@ -0,0 +1,13 @@
+package com.che.comminuty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComminutyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComminutyApplication.class, args);
+    }
+
+}

6e03f560627aa58e6a34fc42f8d2434ca15562d2
captainche1 <807100784v@gmail.com>
2019-08-13 16:03:43 +0800

    flyway

--- src/main/java/com/che/comminuty/mapper/UserMapper.java
+++ src/main/java/com/che/comminuty/mapper/UserMapper.java
@@ -4,10 +4,14 @@ package com.che.comminuty.mapper;
 import com.che.comminuty.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into USER (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("select * from USER where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
